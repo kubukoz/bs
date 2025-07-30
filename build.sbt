708586b69a90ea6a3e6bd0ef125f34b51fb83fc2
Jakub Kozłowski <kubukoz@gmail.com>
2025-07-30 03:20:24 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,29 @@
+ThisBuild / scalaVersion := "3.7.2-RC2"
+ThisBuild / organization := "com.example"
+
+lazy val root = (project in file("."))
+  .enablePlugins(Smithy4sCodegenPlugin, JavaAppPackaging)
+  .settings(
+    name := "bs",
+    libraryDependencies ++= Seq(
+      // Compiler plugins
+      compilerPlugin("org.polyvariant" % "better-tostring" % "0.3.17" cross CrossVersion.full),
+
+      // CLI dependencies
+      "com.monovore" %% "decline" % "2.4.1",
+      "com.monovore" %% "decline-effect" % "2.4.1",
+      "com.indoorvivants" %% "decline-derive" % "0.3.1",
+      "org.typelevel" %% "cats-effect" % "3.5.4",
+
+      // File system operations
+      "com.lihaoyi" %% "os-lib" % "0.10.7",
+
+      // Smithy4s
+      "com.disneystreaming.smithy4s" %% "smithy4s-core" % smithy4sVersion.value,
+
+      // Testing
+      "org.typelevel" %% "weaver-cats" % "0.9.3" % Test,
+      "org.typelevel" %% "weaver-scalacheck" % "0.9.3" % Test,
+    ),
+    Compile / run / fork := true,
+  )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,102 @@
+package com.example.bs
+
+import cats.data.Validated
+import cats.data.ValidatedNel
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.effect.IOApp
+import cats.syntax.all.*
+import com.monovore.decline.*
+import com.monovore.decline.effect.*
+import decline_derive.CommandApplication
+import decline_derive.Positional
+import os.FilePath
+
+given Argument[FilePath] =
+  new Argument[FilePath] {
+    def read(string: String): ValidatedNel[String, FilePath] =
+      Validated
+        .catchNonFatal(os.FilePath(string))
+        .leftMap(_.getMessage)
+        .toValidatedNel
+
+    def defaultMetavar: String = "path"
+  }
+
+enum Command derives CommandApplication {
+  case Build(@Positional("path") path: FilePath)
+  case Dev(@Positional("path") path: FilePath)
+  case Run(@Positional("path") path: FilePath)
+}
+
+object Main
+  extends CommandIOApp(
+    name = "bs",
+    header = "Build system CLI tool",
+    version = "0.1.0",
+  ) {
+
+  override def main: Opts[IO[ExitCode]] = {
+    val pathArg = Opts
+      .argument[FilePath]("path")
+      .withDefault(os.pwd)
+
+    val buildCommand =
+      Opts.subcommand("build", "Build the project") {
+        pathArg.map(Command.Build.apply)
+      }
+
+    val devCommand =
+      Opts.subcommand("dev", "Run in development mode") {
+        pathArg.map(Command.Dev.apply)
+      }
+
+    val runCommand =
+      Opts.subcommand("run", "Run the project") {
+        pathArg.map(Command.Run.apply)
+      }
+
+    // Default to build command when no subcommand is provided
+    val defaultBuild = pathArg.map(Command.Build.apply)
+
+    val command =
+      buildCommand <+>
+        devCommand <+>
+        runCommand <+>
+        defaultBuild
+
+    command.map(executeCommand)
+  }
+
+  private def executeCommand(command: Command): IO[ExitCode] =
+    command match {
+      case Command.Build(path) => BuildCommand.run(path)
+      case Command.Dev(path)   => DevCommand.run(path)
+      case Command.Run(path)   => RunCommand.run(path)
+    }
+
+}
+
+object BuildCommand {
+
+  def run(path: FilePath): IO[ExitCode] = IO
+    .println(s"Build command - path: $path")
+    .as(ExitCode.Success)
+
+}
+
+object DevCommand {
+
+  def run(path: FilePath): IO[ExitCode] = IO
+    .println(s"Dev command - path: $path")
+    .as(ExitCode.Success)
+
+}
+
+object RunCommand {
+
+  def run(path: FilePath): IO[ExitCode] = IO
+    .println(s"Run command - path: $path")
+    .as(ExitCode.Success)
+
+}
