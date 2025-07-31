8b3b826640128f5a6bce7ad929c1805c10edc497
Jakub Kozłowski <kubukoz@gmail.com>
2025-07-31 03:05:03 +0200

    build refactor

--- derivation.nix
+++ derivation.nix
@@ -3,7 +3,7 @@
 let
   bs = {
     build = { src, scalaVersion, libraryDependencies ? [ ]
-      , compilerPlugins ? [ ], ... }@args:
+      , compilerPlugins ? [ ], mainClass ? "", ... }@args:
       let
         classpathFrom = key:
           let
@@ -16,38 +16,42 @@ let
               new dotty.tools.dotc.Driver().main(args);
             }
           }'';
-      in stdenv.mkDerivation ((builtins.removeAttrs args [
-        "buildInputs"
-        "buildPhase"
-        "installPhase"
-        "scalaVersion"
-        "libraryDependencies"
-        "compilerPlugins"
-      ]) // {
-        classpath = classpathFrom "libraryDependencies";
-        compilerClasspath = classpathFrom "compiler";
-        pluginsClasspath = classpathFrom "compilerPlugins";
+        myArgs = {
+          classpath = classpathFrom "libraryDependencies";
+          compilerClasspath = classpathFrom "compiler";
+          pluginsClasspath = classpathFrom "compilerPlugins";
+          inherit src;
 
-        buildInputs = [ openjdk makeWrapper ];
-        buildPhase = ''
-          # set -x
-          echo $depDerivations
-          echo "${compilerInterface}" > CompilerInterface.java
-          javac -cp $compilerClasspath CompilerInterface.java
-          mkdir -p $out/bin
-          java -cp $compilerClasspath:. CompilerInterface -cp $classpath $src/*.scala -Xplugin:$pluginsClasspath -d $out/bin/bs.jar
-        '';
+          buildInputs = [ openjdk makeWrapper ];
+          buildPhase = ''
+            # set -x
+            echo $depDerivations
+            echo "${compilerInterface}" > CompilerInterface.java
+            javac -cp $compilerClasspath CompilerInterface.java
+            mkdir -p $out/bin
+            java -cp $compilerClasspath:. CompilerInterface -cp $classpath $src/*.scala -Xplugin:$pluginsClasspath -d $out/bin/bs.jar
+          '';
 
-        installPhase = ''
-          jar xf $out/bin/bs.jar META-INF/MANIFEST.MF
-          mainClass=$(cat META-INF/MANIFEST.MF | grep Main-Class | cut -d' ' -f2 | tr -d '\r')
-          makeWrapper ${openjdk}/bin/java $out/bin/bs --add-flags "-cp $classpath:$out/bin/bs.jar $mainClass"
-        '';
+          installPhase = ''
+            jar xf $out/bin/bs.jar META-INF/MANIFEST.MF
+            mainClass=$(cat META-INF/MANIFEST.MF | grep Main-Class | cut -d' ' -f2 | tr -d '\r')
+            makeWrapper ${openjdk}/bin/java $out/bin/bs --add-flags "-cp $classpath:$out/bin/bs.jar $mainClass"
+          '';
 
-        meta.buildDefinition = {
-          inherit scalaVersion libraryDependencies compilerPlugins;
+          meta.buildDefinition = {
+            inherit scalaVersion libraryDependencies compilerPlugins;
+          };
         };
-      });
+        finalArgs = (builtins.removeAttrs args [
+          "src"
+          "buildInputs"
+          "buildPhase"
+          "installPhase"
+          "scalaVersion"
+          "libraryDependencies"
+          "compilerPlugins"
+        ]) // myArgs;
+      in stdenv.mkDerivation finalArgs;
   };
 
   scalaVersion = "3.7.2-RC2";
